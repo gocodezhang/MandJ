e8e54127bff4103fe0129454880fa8afb9df6874
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-08-09 09:55:15 -0700

    backend setup

---
+++ Backend/src/main/java/dev/jayzhang/FamilySpaceAPI/FamilySpaceApiApplication.java
@@ -0,0 +1,13 @@
+package dev.jayzhang.FamilySpaceAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilySpaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilySpaceApiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/dev/jayzhang/FamilySpaceAPI/FamilySpaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jayzhang.FamilySpaceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilySpaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
