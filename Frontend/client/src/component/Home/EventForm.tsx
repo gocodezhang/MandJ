cb0552306c84c088ce4478c4de40bf6a79e1a1ec
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-08-30 22:38:58 -0700

    form control

--- Frontend/client/src/component/Home/EventForm.tsx
+++ Frontend/client/src/component/Home/EventForm.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import $ from "jquery";
 import Form from 'react-bootstrap/Form';
 import Modal from "react-bootstrap/Modal";
 import Button from 'react-bootstrap/Button';
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { AppContext } from "../App";
 
 type Props = {
   showForm: boolean;
@@ -13,19 +15,33 @@ type Props = {
 type FormObject = {
   name: string;
   participants: string[];
-  startTime: Date | null;
-  endTime: Date | null;
+  startTime: string;
+  endTime: string;
   location: string;
 }
 
 function EventForm({ showForm, setShowForm}: Props) {
+  const { family } = useContext(AppContext);
   const [formObj, setFormObj] = useState<FormObject>({
     name: '',
     participants: [],
-    startTime: null,
-    endTime: null,
+    startTime: '',
+    endTime: '',
     location: '',
   });
+
+  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    var value;
+    if (e.target.name == 'participants') {
+      value = $("#participants").val();
+    } else {
+      value = e.target.value;
+    }
+    setFormObj({
+      ...formObj,
+      [e.target.name]: value,
+    })
+  }
 
   return (
     <Modal centered show={showForm} onHide={() => (setShowForm(false))}>
@@ -36,25 +52,29 @@ function EventForm({ showForm, setShowForm}: Props) {
         <Form>
           <Form.Group controlId="event-title">
             <Form.Label>Event Name</Form.Label>
-            <Form.Control type="text" name="name"/>
+            <Form.Control type="text" name="name" value={formObj.name} onChange={onChangeHandler} />
           </Form.Group>
           <Form.Group controlId="event-participant">
             <Form.Label>Participants</Form.Label>
-            <Form.Control type="text" placeholder="Add names separated by comma" name="participants"/>
+            <Form.Select id="participants" name="participants" multiple value={formObj.participants} onChange={onChangeHandler}>
+              {family.users.map((user) => (
+                <option value={user.id} key={user.id}>{`${user.firstName} ${user.lastName}`}</option>
+              ))}
+            </Form.Select>
           </Form.Group>
           <Row>
             <Form.Group as={Col} controlId="event-st">
               <Form.Label>Start Time</Form.Label>
-              <Form.Control type="datetime-local" name="startTime"/>
+              <Form.Control type="datetime-local" name="startTime" value={formObj.startTime} onChange={onChangeHandler}/>
             </Form.Group>
             <Form.Group as={Col} controlId="event-et">
               <Form.Label>End Time</Form.Label>
-              <Form.Control type="datetime-local" name="endTime"/>
+              <Form.Control type="datetime-local" name="endTime" value={formObj.endTime} onChange={onChangeHandler}/>
             </Form.Group>
           </Row>
           <Form.Group controlId="event-location">
             <Form.Label>Location</Form.Label>
-            <Form.Control type="text" name="location"/>
+            <Form.Control type="text" name="location" value={formObj.location} onChange={onChangeHandler}/>
           </Form.Group>
         </Form>
       </Modal.Body>
